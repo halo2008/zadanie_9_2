5da018e4e04a6f6e8ebc44281876e88b980cf8a9
halo2008 <ksedkowski@gmail.com>
2017-05-03 08:02:01 +0200

    wprowadzenie kodu

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
-var a = prompt('podaj wartosc a'),
-    b = prompt('podaj wartosc b'),
-    value = (a * a) + (2 * a * b) - (b * b);
-//alert ('wynik ' + value);
-console.log('wynik operacji ' + value);
-if (value > 0) {
-  console.log('wynik dodatni');
-} else if (value < 0) {
-  console.log('wynik ujemny');
-} else {
-  console.log('wynik rowny zero');
-} 
+function getTriangleArea(a,h){
+
+  if (a<=0 || h<=0){
+    console.log('błedna wartosc');
+    return;
+  }
+  else if (a>0 && h>0){
+    var value = (a*h)/2;
+    console.log('pole trojkata ' + value)
+    return;
+  }
+}
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(2, 15);
+var triangle3Area = getTriangleArea(3, 15);
+console.log(getTriangleArea());
