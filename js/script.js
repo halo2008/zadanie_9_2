30605c8e95634fd3c41aaaa0af0bd9614d697523
halo2008 <ksedkowski@gmail.com>
2017-05-07 14:57:38 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-var allNames =[];
-var allNames = allNames.concat(femaleNames,maleNames);
+var allOldNames =[];
+var allNames = allOldNames.concat(femaleNames,maleNames);
 var newName = prompt();
 
-if (allNames.indexOf(newName) === -1){
+if ( allNames.indexOf(newName) === -1 ) {
  allNames.push(newName);
  console.log(allNames); 
 }
